f4eae843d1b892eee361cc26ca7635929692e87d
Michal Janiec <michal.janiec95@gmail.com>
2021-05-17 23:37:06 +0200

    Added AddTaskForm to display

--- src/views/App.js
+++ src/views/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { GlobalStyle } from 'assets/GlobalStyles/GlobalStyles.js';
+import { ThemeProvider } from 'theme-ui';
+import { Box } from 'theme-ui';
 import { theme } from 'assets/theme/theme.js';
-import { ThemeProvider } from 'styled-components';
-
+import MainTemplate from 'components/templates/MainTemplate';
+import AddTaskForm from 'components/organisms/AddTaskForm';
 
 function App() {
   return (
-        <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-         <h1>Hello World!</h1>
-        </>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <>
+        <MainTemplate>
+          <AddTaskForm />
+          <Box p={4} bg="teal">
+            Hello World!
+          </Box>
+        </MainTemplate>
+      </>
+    </ThemeProvider>
   );
 }
 
