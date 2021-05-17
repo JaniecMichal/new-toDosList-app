9fc37edb10a6f350304f0cd042348032d2c2fdbf
Michal Janiec <michal.janiec95@gmail.com>
2021-05-17 23:36:33 +0200

    Created MainTemplate to wrap rest elements

---
+++ src/components/templates/MainTemplate/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Grid } from 'theme-ui';
+
+const MainTemplate = ({ children }) => {
+  return (
+    <Grid
+      as="main"
+      gap={30}
+      columns={['1fr 2fr']}
+      p={10}
+      bg="mercury"
+      sx={{
+        height: '100vh',
+      }}
+    >
+      {children}
+    </Grid>
+  );
+};
+
+export default MainTemplate;
