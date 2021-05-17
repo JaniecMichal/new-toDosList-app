b0f77f724b528b37b137f20c680b75d46e1841c7
Michal Janiec <michal.janiec95@gmail.com>
2021-05-17 23:36:48 +0200

    Created FormField as element of form

---
+++ src/components/molecules/FormField/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box, Input, Label } from '@theme-ui/components';
+
+const FormField = ({ label, name, asWhat = 'input' }) => {
+  return (
+    <Box mb={12}>
+      <Label htmlFor={name} mb={1}>
+        {label}
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        as={asWhat}
+        px={10}
+        py={12}
+        variant={asWhat === 'textarea' ? 'forms.textarea' : 'forms.input'}
+        sx={{
+          resize: 'none',
+          lineHeight: 1.25,
+        }}
+      />
+    </Box>
+  );
+};
+
+export default FormField;
