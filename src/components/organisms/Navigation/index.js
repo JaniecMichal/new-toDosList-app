dbe3aecb96fa5f7f385e06b2b42ad8c843ea0e26
Michal Janiec <michal.janiec95@gmail.com>
2021-05-20 00:07:34 +0200

    Changed theme-ui component to react-router-component. Added styles

--- src/components/organisms/Navigation/index.js
+++ src/components/organisms/Navigation/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Box, NavLink } from '@theme-ui/components';
+import { NavLink } from 'react-router-dom';
+import { Box, Text } from '@theme-ui/components';
 import { toAddNewTask, toTaskListView } from 'assets/helpers/routes';
 
 const Navigation = () => {
   return (
-    <Box as="nav">
-      <NavLink href={toAddNewTask()}>Add new task</NavLink>
-      <NavLink href={toTaskListView()}>Your task list</NavLink>
+    <Box
+      as="nav"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        a: {
+          textDecoration: 'none',
+          marginBottom: 12,
+        },
+      }}
+    >
+      <NavLink to={toAddNewTask()}>
+        <Text variant="text.navLink">Add new task</Text>
+      </NavLink>
+      <NavLink to={toTaskListView()}>
+        <Text variant="text.navLink">Your task list</Text>
+      </NavLink>
     </Box>
   );
 };
