d23d44a72b6fa57622dc7b847e0a70b724de4dd0
Michal Janiec <michal.janiec95@gmail.com>
2021-05-20 22:10:36 +0200

    added routes for EditTaskView

--- src/assets/helpers/routes.js
+++ src/assets/helpers/routes.js
@@ -1,3 +1,4 @@
 export const toAddNewTask = () => '/add-task';
+export const toEditTask = ({ id } = { id: ':id' }) => `/edit-task/${id}`;
 export const toTaskListView = () => '/tasksList';
 export const toTaskDetails = ({ id } = { id: ':id' }) => `/task/${id}`;
