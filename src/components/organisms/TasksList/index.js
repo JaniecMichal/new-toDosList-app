fe67adbfd031121a8f5ea372746f2276cacb6834
Michal Janiec <michal.janiec95@gmail.com>
2021-05-20 19:56:23 +0200

    Added filter feature

--- src/components/organisms/TasksList/index.js
+++ src/components/organisms/TasksList/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Box, Grid } from 'theme-ui';
+import { Box, Grid, Button } from 'theme-ui';
+import { useRecoilState } from 'recoil';
 import SubHeader from 'components/atoms/SubHeader';
 import ListItem from 'components/molecules/ListItem';
+import { hideDoneTasks } from 'recoilElements/atoms';
 
 const TasksList = ({ tasks }) => {
+  const [filter, setFilter] = useRecoilState(hideDoneTasks);
+
+  const updateFilter = () => {
+    setFilter(!filter);
+  };
+
   return (
     <Box
       sx={{
@@ -11,9 +19,24 @@ const TasksList = ({ tasks }) => {
       }}
     >
       <SubHeader>Task to do</SubHeader>
+      <Box my={12}>
+        <Button
+          onClick={updateFilter}
+          sx={{
+            bg: 'teal',
+            marginRight: 2,
+            '&:hover': {
+              cursor: 'pointer',
+              bg: 'bombay',
+            },
+          }}
+        >
+          {filter ? 'Hide done tasks' : 'Show all tasks'}
+        </Button>
+      </Box>
       <Grid as="ul" columns={['1fr 1fr 1fr 1fr']}>
         {tasks.map((task) => (
-          <ListItem key={task.id} taskTitle={task.title} isDone={task.done} />
+          <ListItem key={task.id} task={task} />
         ))}
       </Grid>
     </Box>
