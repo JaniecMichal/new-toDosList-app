37fa630703e506b2fefcbee0426dfe1d3479c270
Michal Janiec <michal.janiec95@gmail.com>
2021-05-20 19:55:28 +0200

    Made label bolder

--- src/components/molecules/ListItem/index.js
+++ src/components/molecules/ListItem/index.js
@@ -2,8 +2,34 @@ import React from 'react';
 import { Box, Button, Flex, Heading, Link } from '@theme-ui/components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { tasksState } from 'recoilElements/atoms';
+import { useRecoilState } from 'recoil';
 
-const ListItem = ({ taskTitle, isDone }) => {
+const ListItem = ({ task }) => {
+  const [tasks, setTasks] = useRecoilState(tasksState);
+  const index = tasks.findIndex(({ id }) => id === task.id);
+
+  const replaceItemAtIndex = (arr, index, newValue) => {
+    return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+  };
+
+  const removeItemAtIndex = (arr, index) => {
+    return [...arr.slice(0, index), ...arr.slice(index + 1)];
+  };
+
+  const toggleDone = () => {
+    const newList = replaceItemAtIndex(tasks, index, {
+      ...task,
+      done: !task.done,
+    });
+    setTasks(newList);
+  };
+
+  const removeTask = () => {
+    const newList = removeItemAtIndex(tasks, index);
+    setTasks(newList);
+  };
+
   return (
     <Flex
       as="li"
@@ -13,16 +39,17 @@ const ListItem = ({ taskTitle, isDone }) => {
         flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'center',
-        bg: 'confetti',
+        bg: `${task.done ? 'scoripion' : 'confetti'}`,
       }}
       mb={14}
     >
       <Heading as="h3" mt={12}>
-        {taskTitle}
+        {task.title}
       </Heading>
       <Link mt={50}>Check details</Link>
       <Box mt={100} sx={{ display: 'flex', alignItems: 'center' }}>
         <Button
+          onClick={toggleDone}
           bg="christi"
           mr={1}
           sx={{
@@ -35,9 +62,10 @@ const ListItem = ({ taskTitle, isDone }) => {
             },
           }}
         >
-          {isDone ? <FontAwesomeIcon icon={faCheck} /> : ''}
+          {task.done ? <FontAwesomeIcon icon={faCheck} /> : ''}
         </Button>
         <Button
+          onClick={removeTask}
           bg="crimson"
           mr={1}
           sx={{
