7ceb7cebc2de500cc2cca03e703259bc493ac98d
Michal Janiec <michal.janiec95@gmail.com>
2021-05-21 23:24:50 +0200

    Added loading data from API.

--- src/views/TasksListView.js
+++ src/views/TasksListView.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { useRecoilValue } from 'recoil';
+import React, { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import TasksList from 'components/organisms/TasksList';
 import { filteredTasksListState } from 'recoilElements/selectors';
+import { useApiData } from 'apiData/useApiData';
+import { Spinner, Text } from '@theme-ui/components';
+import { tasksState } from 'recoilElements/atoms';
 
 const TaskListView = () => {
-  const tasks = useRecoilValue(filteredTasksListState);
+  const [tasks, setTasks] = useRecoilState(tasksState);
+  const filtredTasks = useRecoilValue(filteredTasksListState);
+  const appState = useApiData();
+  console.log(appState);
 
-  return <TasksList tasks={tasks} />;
+  useEffect(() => {
+    if (appState.state === 'sucess') {
+      setTasks(appState.tasks);
+    }
+  }, [appState.state]);
+
+  if (appState.state === 'loading') {
+    return <Spinner />;
+  }
+
+  if (appState.state === 'error') {
+    return (
+      <Text
+        sx={{
+          color: 'crimson',
+          fontWeight: '700',
+        }}
+      >
+        Ups...something went wrong... Please check your network or contact with
+        support mail@example.com 😥
+      </Text>
+    );
+  }
+  return <TasksList tasks={filtredTasks} />;
 };
 
 export default TaskListView;
