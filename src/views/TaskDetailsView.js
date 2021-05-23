fb9524c96a48cae3ae2c8facc3df184786ec4998
Michal Janiec <michal.janiec95@gmail.com>
2021-05-23 21:53:07 +0200

    Adapted to new project. Added new styles. Added warning if something went wrong during rendering

--- src/views/TaskDetailsView.js
+++ src/views/TaskDetailsView.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Box, Text } from '@theme-ui/components';
+import { Box, Text, Flex, Heading } from '@theme-ui/components';
 import { useParams } from 'react-router';
 import { useRecoilValue } from 'recoil';
 import { tasksState } from 'recoilElements/atoms';
 import SubHeader from 'components/atoms/SubHeader';
+import { Link } from 'react-router-dom';
+import { toTaskListView } from 'assets/helpers/routes';
 
 const TaskDetailsView = () => {
   const { id } = useParams();
@@ -11,16 +13,70 @@ const TaskDetailsView = () => {
   const index = tasks.findIndex((task) => task.id === +id);
   const detailedTask = tasks[index];
 
+  if (!tasks.length) {
+    return (
+      <Box
+        sx={{
+          width: '100%',
+          padding: '30px',
+          fontWeight: '700',
+          color: 'crimson',
+          a: {
+            textDecoration: 'none',
+            color: 'pastelGreen',
+
+            '&:hover': {
+              textDecoration: 'underline',
+            },
+          },
+        }}
+      >
+        <Heading mb={10}>
+          Something went wrong... please back to homepage
+        </Heading>
+        <Link to={toTaskListView()}>Back to tasks list</Link>
+      </Box>
+    );
+  }
   return (
-    <Box sx={{ width: '100%' }}>
-      <SubHeader>Details of tasks: {detailedTask.title}</SubHeader>
+    <Box sx={{ width: '100%' }} bg="milkPunch" p={30}>
+      <Flex
+        sx={{
+          width: '100%',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          paddingBottom: 10,
+          borderBottom: '2px solid #222',
+          marginBottom: 12,
+
+          a: {
+            textDecoration: 'none',
+            color: 'pastelGreen',
+            fontWeight: '700',
+
+            '&:hover': {
+              textDecoration: 'underline',
+            },
+          },
+        }}
+      >
+        <SubHeader>Details of task</SubHeader>
+        <Link to={toTaskListView()}>Back to tasks list</Link>
+      </Flex>
       <Text as="p" mt={5}>
-        Task description: {detailedTask.description}
+        Task description:
+        <Text ml={1} sx={{ fontWeight: '700' }}>
+          {detailedTask.title}
+        </Text>
       </Text>
       <Text as="p" mt={5}>
-        completed:
-        <Text ml={1} color={detailedTask.completed ? 'christi' : 'crimson'}>
-          {`${detailedTask.completed}`}
+        Status:
+        <Text
+          ml={1}
+          color={detailedTask.completed ? 'pastelGreen' : 'crimson'}
+          sx={{ fontWeight: '700', textTransform: 'uppercase' }}
+        >
+          {detailedTask.completed ? 'completed' : 'uncompleted'}
         </Text>
       </Text>
     </Box>
