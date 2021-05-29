904ddc1f009dccbe6867e4225dffe9f72c9058bd
Michal Janiec <michal.janiec95@gmail.com>
2021-05-30 00:43:37 +0200

    deleted file and use other version

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -4,12 +4,13 @@ import { useState } from 'react';
 export const useAPI = () => {
   const [response, setResponse] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const endpointURL = 'https://gorest.co.in/public-api/todos';
 
-  const sendRequest = (dataToSend, requestMethod) =>
+  const sendRequest = (dataToSend, requestMethod, id = '') =>
     axios({
       method: requestMethod,
-      url: endpointURL,
+      url: `${endpointURL}/${id}`,
       data: dataToSend,
 
       headers: {
@@ -24,7 +25,21 @@ export const useAPI = () => {
       .catch((err) => {
         console.err(err);
         setLoading(false);
+        setError(err);
       });
 
-  return { sendRequest, response, loading, setLoading };
+  const sendGETRequest = (id = '') =>
+    axios
+      .get(`${endpointURL}/${id}`)
+      .then(({ data }) => {
+        setResponse(data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.err(err);
+        setLoading(false);
+        setError(err);
+      });
+
+  return { sendRequest, response, loading, setLoading, sendGETRequest, error };
 };
