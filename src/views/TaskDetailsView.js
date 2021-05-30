6d722ef105dc7c3591e97317de4ba7ffaa284064
Michal Janiec <michal.janiec95@gmail.com>
2021-05-30 10:41:13 +0200

    Changed condition to render correct elements on page

--- src/views/TaskDetailsView.js
+++ src/views/TaskDetailsView.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Flex, Text, Heading, Spinner } from '@theme-ui/components';
 import { useParams } from 'react-router';
 import SubHeader from 'components/atoms/SubHeader';
@@ -8,18 +8,14 @@ import { useAPI } from 'hooks/useAPI';
 
 const TaskDetailsView = () => {
   const { id } = useParams();
-  const { sendGETRequest, response, loading, setLoading, error } = useAPI();
+  const { sendGETRequest, response, setLoading, error } = useAPI();
 
   useEffect(() => {
     setLoading(true);
     sendGETRequest(id);
-  }, [id, setLoading, sendGETRequest]);
+  }, [id, setLoading]);
 
-  if (loading) {
-    return <Spinner />;
-  }
-
-  if (error || !response) {
+  if (error) {
     return (
       <Box
         sx={{
@@ -45,49 +41,52 @@ const TaskDetailsView = () => {
     );
   }
 
-  return (
-    <Box sx={{ width: '100%' }} bg="milkPunch" p={30}>
-      <Flex
-        sx={{
-          width: '100%',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          paddingBottom: 10,
-          borderBottom: '2px solid #222',
-          marginBottom: 12,
+  if (response) {
+    return (
+      <Box sx={{ width: '100%' }} bg="milkPunch" p={30}>
+        <Flex
+          sx={{
+            width: '100%',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingBottom: 10,
+            borderBottom: '2px solid #222',
+            marginBottom: 12,
 
-          a: {
-            textDecoration: 'none',
-            color: 'pastelGreen',
-            fontWeight: '700',
+            a: {
+              textDecoration: 'none',
+              color: 'pastelGreen',
+              fontWeight: '700',
 
-            '&:hover': {
-              textDecoration: 'underline',
+              '&:hover': {
+                textDecoration: 'underline',
+              },
             },
-          },
-        }}
-      >
-        <SubHeader>Details of task</SubHeader>
-        <Link to={toTaskListView()}>Back to tasks list</Link>
-      </Flex>
-      <Text as="p" mt={5}>
-        Task description:
-        <Text ml={1} sx={{ fontWeight: '700' }}>
-          {response.title}
-        </Text>
-      </Text>
-      <Text as="p" mt={5}>
-        Status:
-        <Text
-          ml={1}
-          color={response.completed ? 'pastelGreen' : 'crimson'}
-          sx={{ fontWeight: '700', textTransform: 'uppercase' }}
+          }}
         >
-          {response.completed ? 'completed' : 'uncompleted'}
+          <SubHeader>Details of task</SubHeader>
+          <Link to={toTaskListView()}>Back to tasks list</Link>
+        </Flex>
+        <Text as="p" mt={5}>
+          Task description:
+          <Text ml={1} sx={{ fontWeight: '700' }}>
+            {response.title}
+          </Text>
         </Text>
-      </Text>
-    </Box>
-  );
+        <Text as="p" mt={5}>
+          Status:
+          <Text
+            ml={1}
+            color={response.completed ? 'pastelGreen' : 'crimson'}
+            sx={{ fontWeight: '700', textTransform: 'uppercase' }}
+          >
+            {response.completed ? 'completed' : 'uncompleted'}
+          </Text>
+        </Text>
+      </Box>
+    );
+  }
+  return <Spinner />;
 };
 
 export default TaskDetailsView;
